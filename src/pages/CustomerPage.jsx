1f759d8bc51a11137a3fb53f69791d9f701ba335
Anouar-91 <a.berrouane@hotmail.com>
2022-10-09 14:08:28 +0200

    factorisation code

--- src/pages/CustomerPage.jsx
+++ src/pages/CustomerPage.jsx
@@ -1,101 +1,104 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios';
 import Pagination from '../components/Pagination';
 
+import CustomersAPI from '../services/CustomersAPI'
 
 export default function CustomerPage() {
 
-  const [currentPage, setCurrentPage] = useState(1);
   const [customers, setCustomers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true)
+  const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState("");
+
+  const fetchCustomers = async () => {
+    try {
+      const data = await CustomersAPI.findAll();
+      setCustomers(data);
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
 
   useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/customers')
-      .then((response) => {
-        console.log(response.data["hydra:member"])
-        setCustomers(response.data["hydra:member"]);
-        setIsLoading(false)
-      })
-      .catch(error => console.log(error.response))
+    fetchCustomers();
   }, [])
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     const copyCustomers = [...customers];
     setCustomers(customers.filter(customer => customer.id !== id))
-    axios.delete('http://127.0.0.1:8000/api/customers/' + id)
-    .then((response) => {
-      console.log(response)
-    })
-    .catch((error) => {
+    try{
+      await CustomersAPI.delete(id);
+      console.log("ok")
+    }catch(error){
       setCustomers(copyCustomers);
       console.log(error.response)
-    })
+    }
   }
-  
+
   const handleChangePage = (page) => {
     setCurrentPage(page)
   }
 
-  
+  const handleSearch = e => {
+    const value = e.currentTarget.value;
+    setSearch(value)
+    setCurrentPage(1)
+
+  }
 
   const itemsPerPage = 10;
 
+  const filteredCustomers = customers.filter(
+    c => 
+    c.firstname.toLowerCase().includes(search.toLowerCase()) ||
+    c.lastname.toLowerCase().includes(search.toLowerCase()) ||
+    c.email.toLowerCase().includes(search.toLowerCase()) ||
+    (c.company &&  c.company.toLowerCase().includes(search.toLowerCase()))
+
+    )
+
   const start = currentPage * itemsPerPage - itemsPerPage
-  const paginatedCustomers = customers.slice(start, start + itemsPerPage);
-
-  const content = isLoading ? (
-    <div className="text-center">
-      <div class="spinner-border" role="status">
-        <span class="sr-only">Loading...</span>
-      </div>
-    </div>
-  ) : (
-    <>
-    <table className="table table-hover table-responsive mt-5">
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Client</th>
-          <th>Email</th>
-          <th>Entreprise</th>
-          <th>Factures</th>
-          <th>Montant total</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {paginatedCustomers.map((customer) =>
-          <tr key={customer.id} >
-            <td>{customer.id}</td>
-            <td><a href="">{customer.lastname} {customer.firstname}</a></td>
-            <td>{customer.email}</td>
-            <td>{customer.company}</td>
-            <td > <span >{customer.invoices.length}</span> </td>
-            <td >{customer.totalAmount.toLocaleString()}€</td>
-            <td>
-            <button 
-                  onClick={() => handleDelete(customer.id)}
-                  disabled={customer.invoices.length>0 ? true : false} 
-                  className="btn btn-sm btn-danger">Supprimer
-                </button>
-            </td>
-          </tr>
-        )}
-
-      </tbody>
-    </table>
-          <Pagination currentPage={currentPage} itemsPerPage={itemsPerPage} length={customers.length} onPageChange={handleChangePage}/>
-          </>
-
-    
-  )
+  const paginatedCustomers = filteredCustomers.slice(start, start + itemsPerPage);
 
   return (
     <>
-      <h1 className="h1">Liste des clients</h1>
+      <h1 class="h1">Liste des clients</h1>
+      <div className="form-group mb-5 mt-5">
+        <input type="text" placeholder="Rechercher..." value={search} onChange={handleSearch} className="form-control" />
+      </div>
+      <table className="table table-hover table-responsive">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Client</th>
+            <th>Email</th>
+            <th>Entreprise</th>
+            <th>Factures</th>
+            <th>Montant total</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {paginatedCustomers.map((customer) =>
+            <tr key={customer.id} >
+              <td>{customer.id}</td>
+              <td><a href="">{customer.lastname} {customer.firstname}</a></td>
+              <td>{customer.email}</td>
+              <td>{customer.company}</td>
+              <td > <span >{customer.invoices.length}</span> </td>
+              <td >{customer.totalAmount.toLocaleString()}€</td>
+              <td>
+                <button
+                  onClick={() => handleDelete(customer.id)}
+                  disabled={customer.invoices.length > 0 ? true : false}
+                  className="btn btn-sm btn-danger">Supprimer
+                </button>
+              </td>
+            </tr>
+          )}
 
-      {content}
-
+        </tbody>
+      </table>
+      <Pagination currentPage={currentPage} itemsPerPage={itemsPerPage} length={filteredCustomers.length} onPageChange={handleChangePage} />
 
     </>
   )
