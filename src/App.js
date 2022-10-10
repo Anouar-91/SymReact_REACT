3af70a69ab4d2628ec9b4d3a7e61e507ab210f08
Anouar-91 <a.berrouane@hotmail.com>
2022-10-10 11:24:51 +0200

    secure api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
-import {  Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter , Navigate, Outlet} from 'react-router-dom';
 import CustomerPage from './pages/CustomerPage';
 import InvoicePage from "./pages/InvoicePage";
 import LoginPage from './pages/LoginPage';
 import AuthAPI from './services/AuthAPI';
-import {useState} from "react";
+import { useState } from "react";
 
 
 AuthAPI.setup();
+
+const ProtectedRoute = ({ isAuthenticated, redirectPath = '/login' }) => {
+  if (!isAuthenticated) {
+      return <Navigate to={redirectPath} replace />;
+  }
+  return <Outlet />;
+};
+
+
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(AuthAPI.isAuthenticated());
 
   return (
 
     <BrowserRouter>
-            <Navbar isAuthenticated={isAuthenticated}   onLogout={setIsAuthenticated}  />
+      <Navbar isAuthenticated={isAuthenticated} onLogout={setIsAuthenticated} />
       <div className="pt-10 pl-10">
         <Routes>
-          <Route path="/customer" element={<CustomerPage />} />
-          <Route path="/invoice" element={<InvoicePage />} />
+          <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+            <Route path="/customer" element={<CustomerPage />} />
+            <Route path="/invoice" element={<InvoicePage />} />
+          </Route>
           <Route path="/login" element={<LoginPage onLogin={setIsAuthenticated} />} />
 
 
